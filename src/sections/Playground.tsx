d83c840a65b92a6a42dbcf9e675ccf227213acc8
Pradip Chapagain <pradipcpgn@gmail.com>
2024-05-06 19:08:27 +0545

    feat:show floor ui

--- src/sections/Playground.tsx
+++ src/sections/Playground.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import ShowLevel from "../components/ShowFloor";
 
 export default function Playground() {
-	return (
-		<div>Playground</div>
-	)
+  return <div className="relative w-full">
+		<ShowLevel/>
+	</div>;
 }
